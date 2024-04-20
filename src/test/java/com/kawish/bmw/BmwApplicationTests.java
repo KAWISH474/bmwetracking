d3eaabda3b8fe37517f275b24bf25f99841f07ce
KAWISH474 <kawishsiddiqui1991@gmail.com>
2024-04-20 12:21:12 +0530

    first commit

---
+++ src/test/java/com/kawish/bmw/BmwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawish.bmw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
